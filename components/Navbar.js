8b5295366f39489f2baffa60712b94d265f9bded
gtallen13 <gtallenpadi13@gmail.com>
2022-03-09 17:44:16 -0600

    Navbar y Footer agregados

---
+++ components/Navbar.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+    return (
+        <>
+            <Head>
+                <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+            </Head>
+            <nav>
+                <div className='logo'>
+                    <h4>CompuTech</h4>
+                </div>
+                <label htmlFor='btn-Menu' className='icon'>
+                    <FontAwesomeIcon icon={faBars} size='2x'/>
+                </label>
+                <input id='btn-Menu' type="checkbox"/>
+                <ul>
+                    <li><Link href="/"><a>Inicio</a></Link></li>
+                    <li><Link href="/Tours/"><a>Tienda</a></Link></li>
+                    <li><Link href="/Tours/"><a>Iniciar Sesion</a></Link></li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
