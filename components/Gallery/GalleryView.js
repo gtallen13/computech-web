62c940f56ea86f4e21d12c19ef567eb8ef9dd28d
gtallen13 <gtallenpadi13@gmail.com>
2022-03-22 12:32:56 -0600

    collage agg

---
+++ components/Gallery/GalleryView.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from "../../styles/Home.module.css"
+import GalleryImage from './GalleryImage';
+const GalleryView = () => {
+    const imagesSrcs = ['gallery-image1.jpg','gallery-image2.jpg','gallery-image3.jpg','gallery-image4.jpg']
+    return (
+        <div className={styles.galleryContainer}>
+            {imagesSrcs.map((image,key)=>(<GalleryImage imgSrc={image} key={key}/>))}
+        </div>
+    );
+}
+
+export default GalleryView;
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 import { Pagination,Navigation } from 'swiper'
 import InfoSlide from '../components/InfoSlide'
+import GalleryView from '../components/Gallery/GalleryView'
 
 export default function Home() {
   return (
@@ -50,16 +51,22 @@ export default function Home() {
 
       {/* About y contacto */}
       <section className={styles.aboutContactSection}>
-        <div className={styles.about}>
+        <div>
           <h3>Acerca de Nosotros</h3>
-          <p>
-            Somos una empresa hondureña ubicados en La Ceiba, Atlantida que tiene como objetivo brindar el mejor servicio y los mejores productos de computacion a nuestros clientes. Contamos con los mejores productos a un precio muy competitivo para que puedas armar tu PC o para que puedas incrementar la potencia de tu PC. No esperes mas y ponte a comprar con CompuTech!
-          </p>
+          <div className={styles.info}>
+            <Image src={'/about-pic.jpg'} width={1000} height={630} alt="about-pic"/>
+            <p>
+              Somos una empresa hondureña ubicados en La Ceiba, Atlantida que tiene como objetivo brindar el mejor servicio y los mejores productos de computacion a nuestros clientes. Contamos con los mejores productos a un precio muy competitivo para que puedas armar tu PC o para que puedas incrementar la potencia de tu PC. No esperes mas y ponte a comprar con CompuTech!
+            </p>
+          </div>
         </div>
-        <div styles={styles.contacto}>
+        <div>
           <h3>Contacto</h3>
-          <p>Correo: infocomputech@computech.com</p>
-          <p>Telefono: 9920-8910</p>
+          <GalleryView/>
+          <div className={styles.info}>
+            <p>Correo: infocomputech@computech.com</p>
+            <p>Telefono: 9920-8910</p>  
+          </div>
         </div>
       </section>
     </div>
